60b12c0514c564c64c19a2f459a7a6c774fc3029
sonuprasad66 <sp.srp1999@gmail.com>
2022-10-28 18:43:20 +0530

    weather_app

---
+++ weather_app/src/App.js
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import { Weather } from "./components/weather";
+
+function App() {
+  return (
+    <Box fontFamily="poppins">
+      <Weather />
+    </Box>
+  );
+}
+
+export default App;
